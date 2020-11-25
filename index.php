305c66a012fef3f0d3628ac91938ca81497b544e
Yord1412 <71461787+Yord1412@users.noreply.github.com>
2020-11-24 19:52:06 -0700

    primer commit
    Este es el inicio, faltan poner bastantes estilos en distintos lugares y crear las otras pantallas, también me faltara implementar la conexión de la base de datos.

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html >
+	<head>
+		<meta charset="utf-8">
+		<title>Examen</title> 
+		
+		<link rel="stylesheet" type="text/css" href="./assets/css/app.css?v=0.0.2" media="all">
+		
+	</head>
+	 
+	<body>
+		<div id="principal">
+			<!-- Menu suoerior estatico -->
+			<div id="menu_estatico">
+				<div id="margen_estatico">
+					<div id="icono_menu">
+						<div id="logomenu">
+							<img src="assets/imagenes/logo.png">
+						</div>
+						<div id="icono_menu_estatico">
+							<a href="#principal">Peli.com</a>
+						</div>
+						<div id="busqueda">
+							<input type="text" name="busqueda" placeholder="buscar pelicula" size="45">
+							<a href="#">GO</a>
+						</div>
+					</div>
+					<div id="botones_menu">
+						<div>
+							<a href="#informacion">Inicio</a>
+						</div>
+						<div>
+							<a href="#referencias">Peliculas</a>
+						</div>
+						<div>
+							<a href="#planes_prin">Shows de TV</a>
+						</div>
+						<div>
+							<a href="#contactanos">Ver en linea</a>
+						</div>
+						<div>
+							<a href="#">Perfil</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- lugar imagen -->
+			<div id="logo">	
+				<img src="assets/imagenes/inicio_imagen.jpg">
+			</div>
+
+			<!-- seccion de cosas a hacer -->
+			<div id="cosas_hacer">
+				<div class="subs">
+					<h2>
+						Peliculas destacadas
+					</h2>
+					<p>
+						de la semana
+					</p>
+				</div>
+				<div id="cuadro-caract">
+					<div id="cuadro">
+						<div class="caracteristicas">
+							<img src="assets/imagenes/xbox one.jpg">
+							<h4>pelicula</h4>
+							<p>
+								20/10/2020
+							</p>
+						</div>
+						<div class="caracteristicas">
+							<img src="assets/imagenes/ps4.jpg">
+							<h4>pelicula</h4>
+							<p>
+								20/10/2020
+							</p>
+						</div>
+						<div class="caracteristicas">
+							<img src="assets/imagenes/swich.jpg">
+							<h4>pelicula</h4>
+							<p>
+								20/10/2020
+							</p>
+						</div>
+						<div class="caracteristicas">
+							<img src="assets/imagenes/pc.jpg">
+							<h4>pelicula</h4>
+							<p>
+								20/10/2020
+							</p>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+
+			<!-- informacion de la pagina -->
+
+			<div id="enlaces_pie">
+				<ul>
+					<li>
+						<img src="assets/imagenes/facebook.png">
+					</li>
+					<li>
+						<img src="assets/imagenes/twitter.png">
+					</li>
+				</ul>
+			</div>
+			<!-- pie de pagina -->
+			<div id="pie_pagina">
+				<div id="elementos">
+					<div id="copyright">
+						<p>
+							©Copyright 2020 peli
+						</p>
+					</div>
+					<div id="botones_menu">
+						<div>
+							<a href="#informacion">Inicio</a>
+						</div>
+						<div>
+							<a href="#referencias">Peliculas</a>
+						</div>
+						<div>
+							<a href="#planes_prin">Shows de TV</a>
+						</div>
+						<div>
+							<a href="#contactanos">Ver en linea</a>
+						</div>
+						<div>
+							<a href="#">Perfil</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
